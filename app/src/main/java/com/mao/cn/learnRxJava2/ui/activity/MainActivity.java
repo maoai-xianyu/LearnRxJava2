95e00c651efa6f5e45ca63cea621e77636b0d997
zhangkun <194264514@qq.com>
2020-08-14 14:43:56 +0800

    roundcorner

--- app/src/main/java/com/mao/cn/learnRxJava2/ui/activity/MainActivity.java
+++ app/src/main/java/com/mao/cn/learnRxJava2/ui/activity/MainActivity.java
@@ -11,6 +11,8 @@ package com.mao.cn.learnRxJava2.ui.activity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,6 +23,7 @@ import com.mao.cn.learnRxJava2.component.AppComponent;
 import com.mao.cn.learnRxJava2.component.DaggerMainComponent;
 import com.mao.cn.learnRxJava2.contants.ValueMaps;
 import com.mao.cn.learnRxJava2.modules.MainModule;
+import com.mao.cn.learnRxJava2.roundcornerprogressbar.RoundCornerProgressBar;
 import com.mao.cn.learnRxJava2.ui.commons.BaseActivity;
 import com.mao.cn.learnRxJava2.ui.features.IMain;
 import com.mao.cn.learnRxJava2.ui.presenter.MainPresenter;
@@ -51,6 +54,8 @@ public class MainActivity extends BaseActivity implements IMain {
     Button btnDescRxjava;
     @BindView(R.id.btn_desc_image)
     Button btnDescImage;
+    @BindView(R.id.rcpb)
+    RoundCornerProgressBar rcpb;
 
 
     @Override
@@ -68,28 +73,39 @@ public class MainActivity extends BaseActivity implements IMain {
         tvHeaderTitle.setText(getString(R.string.header));
         tvHeaderTitle.setVisibility(View.VISIBLE);
         requestPermission();
+
+
+        rcpb.setMax(100);
+        //rcpb.setBackgroundColor(ContextCompat.getColor(this, R.color.mine_vip_progress_default));
+        rcpb.enableAnimation();
+        rcpb.setRadius((int) dp2px(15));
+        rcpb.setProgressColors(getResources().getIntArray(R.array.movie_progress_gradient));
+    }
+
+    float dp2px(float dp) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics);
     }
 
     @Override
     public void setListener() {
 
         RxView.clicks(btnDescNet).throttleFirst(ValueMaps.ClickTime.BREAK_TIME_MILLISECOND, TimeUnit
-                .MILLISECONDS).subscribe(aVoid -> {
+            .MILLISECONDS).subscribe(aVoid -> {
             startActivity(NetWorkRequestActivity.class);
         }, throwable -> {
             LogU.e(throwable.getMessage());
         });
 
-
         RxView.clicks(btnDescRxjava).throttleFirst(ValueMaps.ClickTime.BREAK_TIME_MILLISECOND, TimeUnit
-                .MILLISECONDS).subscribe(aVoid -> {
+            .MILLISECONDS).subscribe(aVoid -> {
             startActivity(RxJavaLearnActivity.class);
         }, throwable -> {
             LogU.e(throwable.getMessage());
         });
 
         RxView.clicks(btnDescImage).throttleFirst(ValueMaps.ClickTime.BREAK_TIME_MILLISECOND, TimeUnit
-                .MILLISECONDS).subscribe(aVoid -> {
+            .MILLISECONDS).subscribe(aVoid -> {
             startActivity(RetrofitShowContentActivity.class);
         }, throwable -> {
             LogU.e(throwable.getMessage());
@@ -100,9 +116,9 @@ public class MainActivity extends BaseActivity implements IMain {
     @Override
     protected void setupComponent(AppComponent appComponent) {
         DaggerMainComponent.builder()
-                .appComponent(appComponent)
-                .mainModule(new MainModule(this))
-                .build().inject(this);
+            .appComponent(appComponent)
+            .mainModule(new MainModule(this))
+            .build().inject(this);
     }
 
     /**
@@ -111,18 +127,18 @@ public class MainActivity extends BaseActivity implements IMain {
     private void requestPermission() {
 
         new RxPermissions(MainActivity.this)
-                .requestEach(Manifest.permission.WRITE_CALENDAR,
-                        Manifest.permission.READ_CALENDAR,
-                        Manifest.permission.RECORD_AUDIO)
-                .subscribe(permission -> {
-                    if (permission.granted) {
+            .requestEach(Manifest.permission.WRITE_CALENDAR,
+                Manifest.permission.READ_CALENDAR,
+                Manifest.permission.RECORD_AUDIO)
+            .subscribe(permission -> {
+                if (permission.granted) {
 
-                    } else {
-                        if (StringU.equals(permission.name, Manifest.permission.RECORD_AUDIO)) {
-                            onTip("未授予录音权限,将会影响语音朗读");
-                        }
+                } else {
+                    if (StringU.equals(permission.name, Manifest.permission.RECORD_AUDIO)) {
+                        onTip("未授予录音权限,将会影响语音朗读");
                     }
+                }
 
-                }, throwable -> LogU.e("异常"));
+            }, throwable -> LogU.e("异常"));
     }
 }
